5c2cf4a49b6dce08367fee5ac99e6e9d210fc260
Brian E Tovar <brianetovar@gmail.com>
2022-09-07 23:36:24 -0400

    functioning test for basic join lobby

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,6 +2,7 @@ let _single = null
 const uri = 'mongodb://localhost:27017'
 const DB_NAME = process.env.DB_NAME || "villainous"
 const CL_GAME = process.env.CL_GAME || "games_v1"
+const CL_LOBBY = process.env.CL_LOBBY || "lobby_v1"
 const CL_USER = process.env.CL_USER || "users_v1"
 const { MongoClient } = require('mongodb')
 
@@ -15,7 +16,7 @@ class MongoDB {
 		return _single
 	}
 	async connect() {
-	    await this.client.connect()
+		await this.client.connect()
 	}
 	async disconnect() {
 		try {
@@ -28,74 +29,63 @@ class MongoDB {
 		let result = await this.client.db(DB_NAME).command({ ping: 1 })
 		return result
 	}
-	async new_game(gameid=0,players=[]) {
-		let d = g
-		if (gameid == 0) {
-			d.setup.url = Math.random().toString(36).substr(2,6)
-		} else {
-			d.setup.url = gameid
-		}
-		let result = await this.client.db(DB_NAME).collection(CL_GAME).insertOne(d)
+	async test_ready() {
+		await this.client.db(DB_NAME).collection(CL_LOBBY).deleteMany({})
+		await this.client.db(DB_NAME).collection(CL_GAME).deleteMany({})
+		await this.client.db(DB_NAME).collection(CL_USER).deleteMany({})
+	}
+	async create_lobby(options={}) {
+		let result = await this.client.db(DB_NAME).collection(CL_LOBBY).insertOne(options)
+    	return result
+	}
+	async get_lobby(query={}) {
+		const cursor = await this.client.db(DB_NAME).collection(CL_LOBBY).find(query, {'_id':1})
+    	return cursor.toArray()
+	}
+	async del_lobby(gameid=0) {
+		let result = await this.client.db(DB_NAME).collection(CL_LOBBY).deleteOne({'_id': gameid})
+		return result
+	}
+	async join_lobby(gameid=0, userid=0) {
+		// check if user exists
+		if (userid == 0) { return 0 }
+		let useres = await this.client.db(DB_NAME).collection(CL_USER).findOne({'_id': userid})
+		// check lobby data
+		if (gameid == 0) { return 0 }
+		let lobres = await this.client.db(DB_NAME).collection(CL_LOBBY).findOne({'_id': gameid})
+		if (lobres.users.length >= lobres.seats) { return 0 }
+		// add user to lobby
+		let result = await this.client.db(DB_NAME).collection(CL_LOBBY).updateOne({'_id': gameid}, {$push: {'users': userid}})
+		return result
+	}
+	async create_game(game={}) {
+		let result = await this.client.db(DB_NAME).collection(CL_GAME).insertOne(game)
     	return result
 	}
 	async get_game(gameid=0) {
-		let result = await this.client.db(DB_NAME).collection(CL_GAME).findOne({'setup.url':gameid})
+		let result = await this.client.db(DB_NAME).collection(CL_GAME).findOne({'_id': gameid})
 		return result
 	}
 	async del_game(gameid=0) {
-		let result = await this.client.db(DB_NAME).collection(CL_GAME).findOneAndDelete({'setup.url':gameid})
+		let result = await this.client.db(DB_NAME).collection(CL_GAME).deleteOne({'_id': gameid})
 		return result
 	}
-	async up(gameid=0, set={}) {
-		if (set === {}) {
-			return 0
-		} else {
-			let result = await this.client.db(DB_NAME).collection(CL_GAME).updateOne({'setup.url': gameid},set)
-			return result
-		}
+	async up(gameid=0, update={}) {
+		if (update === {}) { return 0 }
+		let result = await this.client.db(DB_NAME).collection(CL_GAME).updateOne({'_id': gameid}, update)
+		return result
 	}
-	async new_user(userid=0) {
-		//let result = await this.client.db(DB_NAME).collection(CL_GAME).findOne({'_id':userid})
+	async create_user(user={}) {
+		let result = await this.client.db(DB_NAME).collection(CL_USER).insertOne(user)
+		return result
 	}
 	async get_user(userid=0) {
-		let result = await this.client.db(DB_NAME).collection(CL_USER).findOne()
-	}
-	async list_games(match={}) {
-		let result = await this.client.db(DB_NAME).collection(CL_GAME).find(match, {'setup.url': 1})
+		let result = await this.client.db(DB_NAME).collection(CL_USER).findOne({'_id':userid})
 		return result
 	}
-}
-
-const g = {
-	'setup': {
-		'title': "Marvel Villainous",
-		'users': [],
-		'url': "",
-		'seats': 2
-	},
-	'decks': {
-		'fate': [],
-		'thanos': [],
-		'ultron': []
-	},
-	'hands': {
-		'queue': [],
-		'thanos': [],
-		'ultron': []
-	},
-	'domains': {
-		'thanos': [],
-		'ultron': []
-	},
-	'events': {
-		'global': [],
-		'thanos': [],
-		'ultron': []
-	},
-	'turns': {
-		'order': [],
-		'index': 0,
-		'count': 0
+	async del_user(userid=0) {
+		let result = await this.client.db(DB_NAME).collection(CL_USER).deleteOne({'_id': userid})
+		return result
 	}
 }
 
--- test/data.test.js
+++ test/data.test.js
@@ -1,43 +1,133 @@
 let mongo = require('../src/mongodb.js')
 let db = new mongo.init()
-//jest.setTimeout(10000)
-const TEST_GAME = 'abcdef'
+let TEST_GAMEID = Math.random().toString(36).substr(2,6)
+let TEST_USERID = Math.floor(Math.random() * 1000)
+let TEST_LOBBYID = 1234
 
 beforeAll( async () => {
     await db.connect()
+    await db.test_ready()
 });
 
-test('ping database', async () => { 
+test('ping database', async () => {
     let result = await db.ping()
     expect(result.ok).toBe(1)
 });
 
-test('add test game', async () => { 
-    let result = await db.new_game(TEST_GAME)
+test('create user', async () => {
+    let result = await db.create_user(TEST_USER)
     expect(result.acknowledged).toBeTruthy()
+    TEST_USERID = result.insertedId
 });
 
-test('get test game', async () => { 
-    let result = await db.get_game(TEST_GAME)
-    expect(result.setup.url).toEqual(TEST_GAME)
+test('create lobby', async () => {
+    let result = await db.create_lobby(TEST_LOBBY)
+    expect(result.acknowledged).toBeTruthy()
+    TEST_LOBBYID = result.insertedId
 });
 
-test('update test game data', async () => { 
-    let result = await db.up(TEST_GAME, {$set: {'decks.fate':[0,1,2,3,4,5,6,7,8,9]}})
+test('get lobby', async () => {
+    let result = await db.get_lobby({})
+    expect(result).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({'_id': TEST_LOBBYID})
+        ])
+    )
+});
+
+test('join lobby', async () => {
+    let result = await db.join_lobby(TEST_LOBBYID, TEST_USERID)
+    expect(result).not.toBe(0)
+    expect(result.acknowledged).toBeTruthy()
+    //expect(result.modifiedCount).toBe(1)
+});
+
+test('create game', async () => {
+    let result = await db.create_game(TEST_GAME)
+    expect(result.acknowledged).toBeTruthy()
+    TEST_GAMEID = result.insertedId
+});
+
+test('get game', async () => {
+    let result = await db.get_game(TEST_GAMEID)
+    expect(result._id).toEqual(TEST_GAMEID)
+});
+
+test('update game', async () => {
+    let result = await db.up(TEST_GAMEID, {$set: {'decks.fate':[0,1,2,3,4,5,6,7,8,9]}})
     expect(result.acknowledged).toBeTruthy()
     expect(result.modifiedCount).toBe(1)
 });
 
-// test('list all games', async () => { 
-//     let result = await db.list_games()
-//     expect(result).toContain(TEST_GAME)
+// test('delete user', async () => {
+//     let result = await db.del_user(TEST_USERID)
+//     expect(result.acknowledged).toBeTruthy()
+//     expect(result.deletedCount).toBe(1)
 // });
 
-test('remove test game', async () => { 
-    let result = await db.del_game(TEST_GAME)
-    expect(result.ok).toBe(1)
-});
+// test('delete lobby', async () => {
+//     let result = await db.del_lobby(TEST_LOBBYID)
+//     expect(result.acknowledged).toBeTruthy()
+//     expect(result.deletedCount).toBe(1)
+// });
+
+// test('delete game', async () => {
+//     let result = await db.del_game(TEST_GAMEID)
+//     expect(result.acknowledged).toBeTruthy()
+//     expect(result.deletedCount).toBe(1)
+// });
 
 afterAll( async () => {
     await db.disconnect()
 });
+
+const TEST_USER = {
+    'username': "guest",
+    'name': {
+        'first': "Alice",
+        'last': "Wright"
+    },
+    'stats': {
+        'wins': 0,
+        'total': 0
+    },
+    'birthday': "1970-01-01T00:00:00+00:00",
+    'gender': "F",
+    'location': {
+        'city': "New York",
+        'country': "NY"
+    }
+}
+
+const TEST_GAME = {
+    'turn': {
+        'order': [],
+        'index': 0,
+        'count': 0
+    },
+    'decks': {
+        'fate': [],
+        'thanos': [],
+        'ultron': []
+    },
+    'hands': {
+        'queue': [],
+        'thanos': [],
+        'ultron': []
+    },
+    'domains': {
+        'thanos': [],
+        'ultron': []
+    },
+    'events': {
+        'global': [],
+        'thanos': [],
+        'ultron': []
+    }
+}
+
+const TEST_LOBBY = {
+    'title': "Marvel Villainous",
+    'users': [],
+    'seats': 2
+}
